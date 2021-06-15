c5f9b29a9eb2ce9dcf1a21a2cda96ea5e5b3a707
OmarMWarraich <earthling986@gmail.com>
2021-06-14 20:56:46 -0400

    Set up src/index.tsx: import Provider, import Store type, Import the store function and state, Create a root component, Generate the store, Render the App

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+/* Make the store available to all container components in the application without passing it explicitly 
+*/
+import { Provider } from 'react-redux';
+
+// Store type from Redux
+import { Store } from 'redux';
+
+// Import the Store funtion and State
+import configureStore, { IAppState } from './store/Store';
+import { getAllCharacters } from './actions/CharacterActions';
+
 import './index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+interface IProps {
+  store: Store<IAppState>;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*
+Create a root component that recieves the store via props
+and wraps the App component with Provider, giving props to containers
+*/
+const Root: React.SFC<IProps> = props => {
+  return (
+    <Provider store={props.store}>
+      <App />
+    </Provider>  
+    );
+}
+
+// Generate the Store
+const store = configureStore();
+store.dispatch(getAllCharacters());
+
+//Render the App
+ReactDOM.render(<Root store={store} />,document.getElementById('root') as HTMLElement);
+
+
